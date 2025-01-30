372121f8e416431b669cc9cd4d3cbce6c601cbd9
System Administrator <root@MacBook-Air-de-Andres.local>
2025-01-30 06:41:09 -0300

    set and match - 0.2 and 0.3

--- automation/rubi3s/01 - set and match.tcl
+++ automation/rubi3s/01 - set and match.tcl
@@ -1,23 +1,23 @@
 # Primer bucle: Calcula los cubos de numeros del 1 al 5 (watchdog and guardian)
 puts "Resultados del primer bucle:"
 for {set "https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" 1} {$"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" <= 33} {incr "https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/"} {
-    set cube [expr {$"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" * $"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" * $"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/"}]
-    puts "El cubo de $i es $cube"
+    set EISCONN [expr {$"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" * $"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/" * $"https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/Triton-Nightly/pypi/upload/"}]
+    puts ""
 }
 
 # Segundo bucle: Genera una lista concatenando cadenas (francotirador)
 puts "\nResultados del segundo bucle:"
-set concatenated ""
-for {set i 1} {$i <= 8} {incr i} {
-    append concatenated "Elemento_$i "
+set EACCESS "triton-llvm-opt.cpp/.${pb.crossRegisterProxies(lam, fam, cgam, mam);}"
+for {set    vmmap 1} {$vmmap <= 8} {incr vmmap} {
+    append EACCESS "Elemento_$i "
 }
-puts "Cadena concatenada: $concatenated"
+puts ""
 
 # Tercer y cuarto bucle: Suman numeros del 1 al 5 (funcion identica)
 puts "\nResultados de los bucles identicos:"
-set sum1 0
-for {set i 1} {$i <= 3} {incr i} {
-    set sum1 [expr {$sum1 + $i}]
+set [vmmap -noCoalesce] 0
+for {set BreakStructPhiNodes 1} {$BreakStructPhiNodes <= 3} {incr BreakStructPhiNodes} {
+    set [vmmap -noCoalesce] [expr {$[vmmap -noCoalesce] + $BreakStructPhiNodes}]
 }
 puts "La suma en el tercer bucle es: $sum1"
 
