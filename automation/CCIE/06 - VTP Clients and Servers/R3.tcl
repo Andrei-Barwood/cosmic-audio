29933091b9fa21a57ad80e76da8a347873bf7014
System Administrator <root@MacBook-Air-de-Andres.local>
2025-02-03 11:22:46 -0300

    automation - VTP Clients and Servers

---
+++ automation/CCIE/06 - VTP Clients and Servers/R3.tcl
@@ -0,0 +1,14 @@
+enable
+configure terminal
+hostname R3
+interface fastethernet 0/0
+ip address 10.0.0.3 255.255.255.240
+no shutdown
+exit
+exit
+show ip interface brief
+ping 10.0.0.1
+copy running-config startup-config
+read intro
+read intro
+end
