62ce2c6d181e5e5244d7779a299878eeb48ba1e9
System Administrator <root@MacBook-Air-de-Andres.local>
2025-02-03 10:50:38 -0300

    automation - standard vlan

---
+++ automation/CCIE/05 - standard VLAN/SW1.tcl
@@ -0,0 +1,33 @@
+enable
+configure terminal
+hostname SW1
+vlan 10
+name SALES
+exit
+vlan 20
+name MANAGERS
+exit
+vlan 30
+name ENGINEERS
+exit
+vlan 40
+name SUPPORT
+exit
+interface fastethernet 0/5
+switchport mode access
+switchport access vlan 10
+exit
+interface fastethernet 0/6
+switchport mode access
+switchport access vlan 20
+exit
+interface fastethernet 0/7
+switchport mode access
+switchport access vlan 30
+exit
+interface fastethernet 0/8
+switchport mode access
+switchport access vlan 40
+end
+read intro
+show vlan brief
