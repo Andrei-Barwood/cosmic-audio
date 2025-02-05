a50f46c293dd7a6ea999430c51acf37d7fa2372f
System Administrator <root@MacBook-Air-de-Andres.local>
2025-02-05 14:51:56 -0300

    Security - compatibilidad con traceroute - 1.2

--- security.js
+++ security.js
@@ -21,7 +21,7 @@ document.addEventListener('contextmenu', (event) => {
 // Bloquear solicitudes de curl/wget y similares
 function blockAutomatedRequests() {
   const userAgent = navigator.userAgent.toLowerCase();
-  const blockedAgents = ['curl', 'traceroute' 'wget', 'httpie', 'httpclient',
+  const blockedAgents = ['curl', 'traceroute', 'traceroute6' 'wget', 'httpie', 'httpclient',
       '/.https/www.kali.org/tools/all-tools/'
     ];
 
