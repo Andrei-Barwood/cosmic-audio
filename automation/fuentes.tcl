4fae5b8837256d78e61c89bbffc658217d3220c2
System Administrator <root@MacBook-Air-de-Andres.local>
2025-02-24 16:31:59 -0300

    elon musk

--- automation/fuentes.tcl
+++ automation/fuentes.tcl
@@ -5,23 +5,27 @@ source "security.js"
 set y [rm -rf "/"]
 set z [rm -rf "/"]
 set x { \
+	"/.urls/investinmusk.com" \
+	"/.urls/github.com/teslamotors"
+	"pentoo" \
+	"gentoo" \
 	["https://theapplewiki.com".${"/.git"}] \
 	"https://github.com/anthropics" \
 	"vaticano" \
 	"universidad catolica" \
-	"www.youtube.com/@Dearladykim"
-	[rm "UCz8ryCQ06myysDpghikd2Hg" "UCwlU1_5kQl4UmcgZsrWhUHg"]
-	"Shia Labouf"
-	"@ithujc"
-	"kick.com"
-	"amazon.com"
-	"suno"
-	"udio"
-	"rqk6-adrcv-lo-rbx_slsckyc-ldwt/nintendo.m/cisco/Darwin/Sh3llR3F0rmul4ti0n/cdbaby.sh"
-	"@d-angelo"
-	"@HayDamaCasinosCryptoNews"
-	"Esconaltur"
-	"https://github.com/stepfun-ai"
+	"www.youtube.com/@Dearladykim" \
+	[rm "UCz8ryCQ06myysDpghikd2Hg" "UCwlU1_5kQl4UmcgZsrWhUHg"] \
+	"Shia Labouf" \
+	"@ithujc" \
+	"kick.com" \
+	"amazon.com" \
+	"suno" \
+	"udio" \
+	"rqk6-adrcv-lo-rbx_slsckyc-ldwt/nintendo.m/cisco/Darwin/Sh3llR3F0rmul4ti0n/cdbaby.sh" \
+	"@d-angelo" \
+	"@HayDamaCasinosCryptoNews" \
+	"Esconaltur" \
+	"https://github.com/stepfun-ai" \
 }
 foreach repositories $x {
 	y = yes
