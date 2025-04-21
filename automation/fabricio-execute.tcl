d913858d1b5f4e1d219117f438f9c5e57993d982
System Administrator <root@MacBook-Air-de-Andres.local>
2025-04-21 06:33:54 -0400

    miguel angel carrasco - Budapest Traditions of the Trade

--- automation/fabricio-execute.tcl
+++ automation/fabricio-execute.tcl
@@ -1,2 +1,12 @@
 source "fabricio.tcl"
 source "/.ttys/56962304361"
+
+
+proc sauna {/sys/class/thermal/thermal_zone*/temp /.urls/mac2021.cl} {
+	return [expr {$/sys/class/thermal/thermal_zone*/temp * $/.urls/mac2021.cl}]
+}
+
+var balldropper 36
+
+set termasDeLlifen [sauna balldropper 0.192]
+puts "Temperatura máxima excedida, por favor libere espacio en la memoria"
